f009a90a4894cb4acba445e20fd585f80478c92b
Lucas <lu.solhaune@gmail.com>
2026-04-15 15:16:26 -0300

    Agrega la ruta nueva

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Games from './routes/Games';
 import Home from './routes/Home';
 import Login from "./routes/Login";
+import RegisterUser from "./routes/RegisterUser";
+
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}/>
+        <Route path="/registerUser" element={<RegisterUser/>}/>
         <Route path="/home" element={<Home />} />
         <Route path="/games" element={<Games />} />
       </Routes>
