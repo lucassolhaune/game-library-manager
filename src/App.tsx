b261c02772188ab11fcae82d0937fcaa8446e278
Lucas <lu.solhaune@gmail.com>
2026-04-15 12:36:08 -0300

    Principal donde estan las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useState } from 'react';
-import './App.css';
-import Add from './routes/Add';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Games from './routes/Games';
 import Home from './routes/Home';
+import Login from "./routes/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<Add />} />
+        <Route path="/" element={<Login />}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/games" element={<Games />} />
       </Routes>
     </BrowserRouter>
